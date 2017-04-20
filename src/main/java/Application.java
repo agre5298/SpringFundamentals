d8e4a3ce6008b8490e640cec75dd6594b69579b9
agre5298 <qubic665@gmail.com>
2017-04-20 14:50:10 +0200

    second

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,40 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
 
 public class Application {
 
 	public static void main(String[] args) {
-		
-		CustomerService service= new CustomerServiceImpl();
-		
-		
+
+		ApplicationContext appcontext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		// CustomerService service= new CustomerServiceImpl();
+		CustomerService service = appcontext.getBean("customerService", CustomerService.class);
+		System.out.println(service);
+		CustomerService service1 = appcontext.getBean("customerService", CustomerService.class);
+		System.out.println(service1);
 		System.out.println(service.findAll().get(0).getFirstname());
-		
-		
-		
-		
 
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,22 +2,30 @@ package com.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
+@Scope("prototype")
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
-
-	/* (non-Javadoc)
-	 * @see com.pluralsight.service.CustomerService#findAll()
-	 */
+	private CustomerRepository customerRepository ;
+	
+	
+	
 	@Override
 	public List<Customer> findAll() {
 
 		return customerRepository.findAll();
 
+	}
+
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
 	}
 
 }
